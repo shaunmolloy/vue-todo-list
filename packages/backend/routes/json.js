89de2ee0e51da4e278b2a0261ad0359ed12f913d
Shaun Molloy <shaunmolloy@gmail.com>
2021-06-04 21:32:16 +0100

    Add json response objects

---
+++ packages/backend/routes/json.js
@@ -0,0 +1,12 @@
+const jsonSuccess = {
+  status: 'success',
+  data: [],
+};
+
+const jsonError = {
+  status: 'error',
+  message: '',
+  errors: [],
+};
+
+module.exports = { jsonSuccess, jsonError };

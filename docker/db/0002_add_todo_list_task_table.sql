242ebfe483e5f79fb84ce6bc26b32d5f9207abd9
Shaun Molloy <shaunmolloy@gmail.com>
2021-06-04 20:33:56 +0100

    Add todo_list_task table sql

---
+++ docker/db/0002_add_todo_list_task_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `todo_list_task`
+(
+    `id` int NOT NULL AUTO_INCREMENT,
+    `list_id` int NOT NULL,
+    `name` varchar(255),
+    `status` varchar(50) DEFAULT  'active',
+    `updated` datetime,
+    `created` datetime,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`list_id`) REFERENCES `todo_list` (`id`)
+);

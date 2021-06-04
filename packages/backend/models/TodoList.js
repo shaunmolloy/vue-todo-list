1b041dc5a9c2399cfafda9c78f0253fddfc65151
Shaun Molloy <shaunmolloy@gmail.com>
2021-06-04 20:35:42 +0100

    Add backend models

---
+++ packages/backend/models/TodoList.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const TodoList = sequelize.define('TodoList', {
+  name: DataTypes.STRING,
+  status: DataTypes.STRING,
+}, {
+  tableName: 'todo_list',
+  createdAt: 'created',
+  updatedAt: 'updated',
+});
+
+module.exports = TodoList;
---
+++ packages/backend/models/TodoListTask.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const TodoListTask = sequelize.define('TodoListTask', {
+  name: DataTypes.STRING,
+  status: DataTypes.STRING,
+}, {
+  tableName: 'todo_list_task',
+  createdAt: 'created',
+  updatedAt: 'updated',
+});
+
+module.exports = TodoListTask;

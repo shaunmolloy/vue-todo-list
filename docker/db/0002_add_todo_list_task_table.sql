dcfa901009f2c9a3b47e58d0d9ab18673d83956e
Shaun Molloy <shaunmolloy@gmail.com>
2021-06-04 21:30:09 +0100

    Set fk name in todo_list_task table

--- docker/db/0002_add_todo_list_task_table.sql
+++ docker/db/0002_add_todo_list_task_table.sql
@@ -7,5 +7,6 @@ CREATE TABLE `todo_list_task`
     `updated` datetime,
     `created` datetime,
     PRIMARY KEY (`id`),
+    CONSTRAINT `fk_todo_list_task_list_id`
     FOREIGN KEY (`list_id`) REFERENCES `todo_list` (`id`)
 );

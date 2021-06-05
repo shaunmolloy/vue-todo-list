aa7903e103b1b4097f1942aa343352daefe81dd8
Shaun Molloy <shaunmolloy@gmail.com>
2021-06-05 23:59:54 +0100

    Update Home to show Tasks

--- packages/frontend/src/js/components/pages/Home.vue
+++ packages/frontend/src/js/components/pages/Home.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="home page">
     <div class="container">
-      Home
+      <tasks />
     </div>
   </div>
 </template>
+
+<script>
+import Tasks from '@pages/tasks/Tasks.vue';
+
+export default {
+  components: {
+    Tasks,
+  },
+}
+</script>
